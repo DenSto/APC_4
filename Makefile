55717b2d5127fc4716b92698865d1af4914df120
DenSto <dstonge@pppl.gov>
2016-11-17 12:11:39 -0500

    Merged openmp and serial files

--- Makefile
+++ Makefile
@@ -2,20 +2,21 @@ objects := $(wildcard *.o)
 libs = -lm
 FLAGS = -g
 op = -O3
+
 CLANG_OMP = ~/openmp/build/bin/clang
 
 heat : heat.o grid.o field.o utils.o
 	cc $(op) -g -o $@ heat.o grid.o field.o utils.o $(libs)
-heat_omp : heat_omp.o grid.o field.o utils.o
-	$(CLANG_OMP) -fopenmp  $(op) -o $@ heat_omp.o grid.o field.o utils.o $(libs)
 
+heat_omp : heat_omp.o grid.o field.o utils.o
+	$(CLANG_OMP) -D OPENMP -fopenmp  $(op) -o $@ heat_omp.o grid.o field.o utils.o $(libs)
 
 all : heat heat_omp
 
 clean : 
 	rm -f $(objects) heat heat_omp
 
+heat_omp.o :
+	cc $(op) -D OPENMP -o heat_omp.o -c $(CFLAGS) heat.c
 %.o : %.c
-	$(CLANG_OMP) -fopenmp $(op) -c $(CFLAGS) $<
-#%.o : %.c
-#	cc -O3 -g -c $(CFLAGS) $<
+	cc $(op)  -c $(CFLAGS) $<
--- heat.c
+++ heat.c
@@ -5,11 +5,15 @@
 #include "grid.h"
 #include "field.h"
 
+#ifdef OPENMP
+#include <omp.h>
+int nthreads=0;
+#endif
+
 #define GHOST_CELLS 1
 #define KAPPA 1.0
 #define OUTPUT_CADENCE 10000
 
-double dx=0;
 
 
 int initialize_problem(int nx, double length, Grid** grid, Field** field, Field** rhs);
@@ -24,13 +28,21 @@ int output_field(Grid* grid, Field* field);
 int main(int argc, char* argv[]){
     
 // Check to make sure usage is correct
+#ifdef OPENMP
+    if(argc != 3){
+	printf("Usage: %s nx nthreads\n",argv[0]);
+	return 1;
+    }
+    nthreads=atoi(argv[2]);
+    omp_set_num_threads(nthreads);
+#else
     if(argc != 2){
 	printf("Usage: %s nx\n",argv[0]);
 	return 1;
     }
+#endif
 
-    int nx  = atoi(argv[1]);
-
+    int nx = atoi(argv[1]);
     long step=0;
 
     double step_time=0.0;
@@ -39,52 +51,56 @@ int main(int argc, char* argv[]){
     tv_st = tv_prev;
 
 
-
-    
-    dx = M_PI/nx;
-
-    double dt = 0.5 * dx * dx / (4*KAPPA);
-    double t = 0;
-    double tF = 0.5 * M_PI * M_PI / KAPPA;
-    printf("Total Time: %f     Time Step:%.2e Steps to take:%d\n",tF,dt, (int) (tF/dt));
-
-
     Grid* grid;
     Field *temp, *rhs;
 
     initialize_problem(nx,M_PI,&grid, &temp, &rhs);
 
-    while(t <= tF){
-        if(step % OUTPUT_CADENCE == 0){
-            gettimeofday(&tv_curr,NULL);
-            step_time = (double)(tv_curr.tv_sec - tv_prev.tv_sec)+
-                    1.0e-6*(double)(tv_curr.tv_usec - tv_prev.tv_usec);
-            tv_prev = tv_curr;
-            printf("Step: %ld   Step time: %.2e s \n",step,step_time);
-        }
 
-        compute_rhs(grid,temp,rhs);
-        time_step(dt,grid,temp,rhs);
-        compute_boundaries(grid,temp);
+    double dt = 0.5 * grid->dx * grid->dy / (4*KAPPA);
+    double t = 0;
+    double tF = 0.5 * M_PI * M_PI / KAPPA;
+    printf("Total Time: %f     Time Step:%.2e Steps to take:%d\n",tF,dt, (int) (tF/dt));
+
+    #pragma omp parallel shared(grid,temp,rhs,tF,tv_curr,tv_prev) firstprivate(step,step_time,t,dt)
+    {
+        while(t <= tF){
+            #pragma omp master
+            {
+                if(step % OUTPUT_CADENCE == 0){
+                    gettimeofday(&tv_curr,NULL);
+                    step_time = (double)(tv_curr.tv_sec - tv_prev.tv_sec)+
+                         1.0e-6*(double)(tv_curr.tv_usec - tv_prev.tv_usec);
+                    tv_prev = tv_curr;
+                    printf("Step: %ld   Step time: %.2e s \n",step,step_time);
+                } 
+            }
+
+            compute_rhs(grid,temp,rhs);
+            time_step(dt,grid,temp,rhs);
+
+            #pragma omp barrier 
+
+            compute_boundaries(grid,temp);
         
-        t += dt;
-        step++;
-
+            t += dt;
+            step++;
+        }
     } 
 
+    double ave_temp = calculate_temperature(grid,temp);
     output_field(grid,temp);
 
-    double ave_temp = calculate_temperature(grid,temp);
 
     gettimeofday(&tv_fin,NULL);
     double total_time = (double)(tv_fin.tv_sec - tv_st.tv_sec)+
                     1.0e-6*(double)(tv_fin.tv_usec - tv_st.tv_usec);
 
     printf("Final temperature:  %e   Total simulation time:%.2e s\n", ave_temp, total_time);
+
     free_grid(grid);
     free_field(temp);
     free_field(rhs);
-
 
 }
 
@@ -104,50 +120,61 @@ int compute_boundaries(Grid* grid, Field* field){
     int i;
     int nx = field->nx; 
     int ny = field->ny; 
-    for(i = 0; i < nx; i++){
-        // Bottom cos(x)**2
-        double x = grid_X(grid,i,-1);
-        set_field_value(field,i,-1,cos(x)*cos(x));
+   
+    {
+    #pragma omp for
+        for(i = 0; i < nx; i++){
+            // Bottom cos(x)**2
+            double x = grid_X(grid,i,-1);
+            set_field_value(field,i,-1,cos(x)*cos(x));
 
-        // Top sin(x)**2
-        x = grid_X(grid,i,ny);
-        set_field_value(field,i,ny,sin(x)*sin(x));
+            // Top sin(x)**2
+            x = grid_X(grid,i,ny);
+            set_field_value(field,i,ny,sin(x)*sin(x));
 
-        // Left - Periodic
-
-        set_field_value(field,-1,i,get_field_value(field,nx-1,i));
+            // Left - Periodic
+            set_field_value(field,-1,i,get_field_value(field,nx-1,i));
     
-        // Right - Periodic
-        set_field_value(field,nx,i,get_field_value(field,0,i));
+            // Right - Periodic
+            set_field_value(field,nx,i,get_field_value(field,0,i));
+        }
     }
     return 1;
 }
 
 int compute_rhs(Grid* grid, Field* field, Field* rhs){
+    int nx = field->nx;
+    int ny = field->ny;
     int i,j;
-    for(i = 0; i < field->nx; i++){
-        for(j = 0; j < field->ny; j++){
-           double val;
+    double val;
+    {
+    #pragma omp for
+    for(i = 0; i < nx; i++){
+        for(j = 0; j < ny; j++){
            val= -4*get_field_value(field,i,j);
            val+= get_field_value(field,i,j+1);
            val+= get_field_value(field,i,j-1);
            val+= get_field_value(field,i-1,j);
            val+= get_field_value(field,i+1,j);
-           val = KAPPA * val/(dx*dx);
+           val = KAPPA * val/(grid->dx*grid->dx);
            set_field_value(rhs,i,j,val);
-        } 
+        }
+      }
     }
     return 1;
 }
 
 int time_step(double dt, Grid* grid, Field* field, Field* rhs){
     int i,j;
+     {  
+    #pragma omp for
     for(i = 0; i < field->nx; i++){
         for(j = 0; j < field->ny; j++){
             double old = get_field_value(field,i,j);
             double new = dt*get_field_value(rhs,i,j);
             set_field_value(field,i,j,old+new);
         }
+      }
     } 
     return 1;
 }
@@ -155,6 +182,7 @@ int time_step(double dt, Grid* grid, Field* field, Field* rhs){
 double calculate_temperature(Grid* grid, Field* field){
     double result=0;
     int i,j;
+    #pragma omp parallel for shared(field,grid) private (i,j) reduction(+:result)
     for(i =0; i < field->nx; i++){
         for(j =0; j < field->ny; j++){
             result += get_field_value(field,i,j);
@@ -165,8 +193,8 @@ double calculate_temperature(Grid* grid, Field* field){
 
 
 int output_field(Grid* grid, Field* field){
-    FILE *fp = fopen("out.dat","w");
     int i,j;
+    FILE *fp = fopen("out.dat","w");
     for(i =0; i <= field->nx; i++){
         for(j =0; j <= field->ny; j++){
             fprintf(fp,"%e %e %e\n",grid_X(grid,i,j),grid_Y(grid,i,j),get_field_value(field, i,j));
@@ -175,7 +203,5 @@ int output_field(Grid* grid, Field* field){
     }
     fclose(fp);
     return 1;
-
-
-
 }
+
